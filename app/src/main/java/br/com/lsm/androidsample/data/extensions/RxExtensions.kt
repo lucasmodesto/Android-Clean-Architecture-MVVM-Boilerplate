f166d6831c64f25f67748a40af1e83c71f750443
Lucas <dev.lucasmodesto@gmail.com>
2020-03-11 17:04:39 -0300

    removed unused imports

--- app/src/main/java/br/com/lsm/androidsample/data/extensions/RxExtensions.kt
+++ app/src/main/java/br/com/lsm/androidsample/data/extensions/RxExtensions.kt
@@ -1,13 +1,9 @@
 package br.com.lsm.androidsample.data.extensions
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import br.com.lsm.androidsample.data.rx.NetworkErrorTransformer
-import br.com.lsm.androidsample.presentation.core.State
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 fun <T> Observable<T>.defaultSchedulers(): Observable<T> {
