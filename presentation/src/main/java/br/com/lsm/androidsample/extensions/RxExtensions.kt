68d43208d08fdc8654c0d3aad58952078b5d669e
Lucas <dev.lucasmodesto@gmail.com>
2020-04-27 17:22:32 -0300

    removed unused imports

--- presentation/src/main/java/br/com/lsm/androidsample/extensions/RxExtensions.kt
+++ presentation/src/main/java/br/com/lsm/androidsample/extensions/RxExtensions.kt
@@ -3,12 +3,9 @@ package br.com.lsm.androidsample.extensions
 import androidx.lifecycle.MutableLiveData
 import br.com.lsm.androidsample.core.State
 import br.com.lsm.androidsample.rx.ISchedulerProvider
-import br.com.lsm.androidsample.rx.SchedulerProvider
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun <T> Single<T>.subscribeWithLiveDataState(liveData: MutableLiveData<State<T>>): Disposable {
     return this.doOnSubscribe { liveData.value = State.Loading(isLoading = true) }
